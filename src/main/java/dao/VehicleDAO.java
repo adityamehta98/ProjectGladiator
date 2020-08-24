b910d3d98a4a02d8987e7c7963813fb9d6005fb1
adityamehta98 <adityamehta1298@gmail.com>
2020-08-24 11:38:38 +0530

    vehicle

--- src/main/java/dao/VehicleDAO.java
+++ src/main/java/dao/VehicleDAO.java
@@ -6,7 +6,7 @@ import model.*;
 public interface VehicleDAO
 {
 	public List<Vehicle> findAll();
-	public User findVehicleByUserId(int userid);
-	public Loan findVehicleByLoanID(int loanid);
+	public Vehicle findVehicleByUserId(int vehicleid);
+	public Vehicle findVehicleByLoanID(int vehicleid);
 
 }
