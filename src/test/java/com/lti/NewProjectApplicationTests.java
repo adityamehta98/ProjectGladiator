c934eee94dc9f58306c7e8a30d4b51efcb5b630a
Kavita Karki <kvtkrk441@gmail.com>
2020-08-24 23:38:03 +0530

    trial!!!

--- src/test/java/com/lti/NewProjectApplicationTests.java
+++ src/test/java/com/lti/NewProjectApplicationTests.java
@@ -21,81 +21,83 @@ import com.lti.repo.RepositoryInterface;
 @AutoConfigureTestDatabase(replace=Replace.NONE)
 @Rollback(false)
 public class NewProjectApplicationTests {
-	//Kavita
+	
 	@Autowired
 	private RepositoryInterface repo;
 	
-	@Test
-	void registerUser() {
-		
-		UserTable user=new UserTable();
-		user.setUserId(1);
-		user.setUserNameFirst("Adi");
-		user.setUserNameLast("Mehta");
-		user.setUserNameMiddle("");
-		user.setUserAge(BigDecimal.valueOf(21));
-		user.setUserGender("Male");
-		user.setUserAddress("221B Bakers street");
-		user.setUserEmail("adya@gmail.com");
-		user.setUserPass("Aditya@123");
-		user.setUserZip("302001");
-		user.setUserType("New");
-		repo.registerUser(user);
-	}
-	
-	@Test
-	void registerAdmin() {
-		Admin admin = new Admin();
-		admin.setAdminId(1001);
-		admin.setAdminEmail("kavaK@gmail.com");
-		admin.setAdminNameFirst("KavitaJi");
-		admin.setAdminNameLast("Karki");
-		admin.setAdminGender("Female");
-		admin.setAdminPass("abcd1234");
-		admin.setAdminAge(BigDecimal.valueOf(21));
-		System.out.println(repo.registerAdmin(admin));
-	}
-	
-	
-	@Test
-	void registerAccount() {
-		Account account= new Account();
-		account.setAccNumber("DCYGR234235");
-		account.setAccBankName("HDFC");
-		account.setAccIfsc("HDF1344");
-		account.setAccType("Corporate");
-		account.setSalary(BigDecimal.valueOf(50000));
-		account.setExisitingEmi(BigDecimal.valueOf(2));
-		account.setUserEmploymentType("Salaried");
-		account.setMonthlySavings(BigDecimal.valueOf(20000));
-		System.out.println(repo.registerAccount(account));
-	}
-	
-	@Test
-	void registerLoan() {
-		Loan loan=new Loan();
-		loan.setLoanId(1);
-		loan.setLoanAmount(BigDecimal.valueOf(50000));
-		loan.setInterestRate(BigDecimal.valueOf(9));
-		loan.setProcessingFee(BigDecimal.valueOf(1000));
-		loan.setEmi(BigDecimal.valueOf(5000));
-		loan.setTenure(BigDecimal.valueOf(36));
-		loan.setApplicationStatus("Complete");
-		loan.setLoanStatus("New");
-		loan.setLoanStartDate(Date.valueOf("2018-09-12"));
-		loan.setLoanEndDate(Date.valueOf("2021-09-12"));
-		System.out.println(repo.registerLoan(loan));
-	}
+//	@Test
+//	void registerUser() {
+//		
+//		UserTable user=new UserTable();
+//		user.setUserId(1);
+//		user.setUserNameFirst("Adi");
+//		user.setUserNameLast("Mehta");
+//		user.setUserNameMiddle("");
+//		user.setUserAge(BigDecimal.valueOf(21));
+//		user.setUserGender("Male");
+//		user.setUserAddress("221B Bakers street");
+//		user.setUserEmail("adya@gmail.com");
+//		user.setUserPass("Aditya@123");
+//		user.setUserZip("302001");
+//		user.setUserType("New");
+//		repo.registerUser(user);
+//	}
+//	
+//	@Test
+//	void registerAdmin() {
+//		Admin admin = new Admin();
+//		admin.setAdminId(1001);
+//		admin.setAdminEmail("kavaK@gmail.com");
+//		admin.setAdminNameFirst("KavitaJi");
+//		admin.setAdminNameLast("Karki");
+//		admin.setAdminGender("Female");
+//		admin.setAdminPass("abcd1234");
+//		admin.setAdminAge(BigDecimal.valueOf(21));
+//		System.out.println(repo.registerAdmin(admin));
+//	}
+//	
+//	
+//	@Test
+//	void registerAccount() {
+//		Account account= new Account();
+//		account.setAccNumber("DCYGR234235");
+//		account.setAccBankName("HDFC");
+//		account.setAccIfsc("HDF1344");
+//		account.setAccType("Corporate");
+//		account.setSalary(BigDecimal.valueOf(50000));
+//		account.setExisitingEmi(BigDecimal.valueOf(2));
+//		account.setUserEmploymentType("Salaried");
+//		account.setMonthlySavings(BigDecimal.valueOf(20000));
+//		System.out.println(repo.registerAccount(account));
+//	}
+//	
+//	@Test
+//	void registerLoan() {
+//		Loan loan=new Loan();
+//		loan.setLoanId(1);
+//		loan.setLoanAmount(BigDecimal.valueOf(50000));
+//		loan.setInterestRate(BigDecimal.valueOf(9));
+//		loan.setProcessingFee(BigDecimal.valueOf(1000));
+//		loan.setEmi(BigDecimal.valueOf(5000));
+//		loan.setTenure(BigDecimal.valueOf(36));
+//		loan.setApplicationStatus("Complete");
+//		loan.setLoanStatus("New");
+//		loan.setLoanStartDate(Date.valueOf("2018-09-12"));
+//		loan.setLoanEndDate(Date.valueOf("2021-09-12"));
+//		System.out.println(repo.registerLoan(loan));
+//	}
 //	
 //	@Test
 //	void registerVehicle() {
 //		Vehicle vehicle=new Vehicle();
 //		vehicle.setVehicleId(21234164);
-//		vehicle.setve("Maruti Suzuki Swift");
-//		vehicle.setVehiclemodel("VXI");
-//		vehicle.setVehicletype("Car");
-//		vehicle.setExshowroomprice("700000");
-//		vehicle.setOnroadprice("760000");
+//		vehicle.setVehicleMake("Maruti Suzuki Swift");
+//		vehicle.setVehicleModel("VXI");
+//		vehicle.setVehicleType("Car");
+//		vehicle.setVehicleYear(BigDecimal.valueOf(700000));
+//		vehicle.setVehicleColour("Navy Blue");
+//		vehicle.setExShowroomPrice(BigDecimal.valueOf(700000));
+//		vehicle.setOnRoadPrice(BigDecimal.valueOf(760000));
 //		System.out.println(repo.registerVehicle(vehicle));
 //	}
 }
