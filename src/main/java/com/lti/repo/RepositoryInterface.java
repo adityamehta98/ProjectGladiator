c267045254658754a9c566984f3ddbc447c73cb5
RichAryaG <gajendrar.jnv@gmail.com>
2020-08-25 23:22:23 +0530

    Repo Interface

--- src/main/java/com/lti/repo/RepositoryInterface.java
+++ src/main/java/com/lti/repo/RepositoryInterface.java
@@ -23,7 +23,8 @@ public interface RepositoryInterface {
 	
 	public long registerAdmin(Admin admin); //register an admin
 	boolean isAdminPresent(String adminEmail); //admin login (1)
-	public long findAdminIdByEmailAndPassword(String adminEmail, String adminPass); // admin login (2)
+	public long findAdminIdByEmailAndPassword(String adminEmail, String userPass);
+	public Admin findAdminById(long adminId); // admin login (2)
 	
 	public List<UserTable> fetchAllRegisteredUsers(); // find all registered users 
 
