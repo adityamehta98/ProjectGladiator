f1158946ac692300015406bcbb2129ee49ba5ed7
RichAryaG <gajendrar.jnv@gmail.com>
2020-08-25 23:30:20 +0530

    All Things Implemented Aditya and Kavita can refer this commit.

--- src/main/java/com/lti/Dto/AdminLoginDto.java
+++ src/main/java/com/lti/Dto/AdminLoginDto.java
@@ -2,20 +2,20 @@ package com.lti.Dto;
 
 public class AdminLoginDto {
 
-	private int adminId;
+	private String email;
 	private String password;
-	public int getAdminId() {
-		return adminId;
-	}
-	public void setAdminId(int adminId) {
-		this.adminId = adminId;
-	}
+	
 	public String getPassword() {
 		return password;
 	}
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
-	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 }
